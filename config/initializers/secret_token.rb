600946e1b9b151ad12c49d1401f6e94ac02ede2b
magicHu <huronghai2008@gmail.com>
2013-06-18 19:01:32 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BsharePoints::Application.config.secret_token = '2a4c30c77584d710f25c1714bda4f874c6f678e2c9e6d8921d7f4aaf7044ab09d2b1b478827947dfdb69b644c630d8845fef43b72ab70427fca3b641473ba228'
